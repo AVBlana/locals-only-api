0b938eb90f6e3eb05db736e4d339699e4a2b4d0f
AVBlana <71121584+AVBlana@users.noreply.github.com>
2023-11-24 21:13:09 +0200

    add backend environments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -40,36 +37,41 @@ const resolvers = {
   },
 };
 
+import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
 import http from "http";
 import cors from "cors";
 
-interface MyContext {
-  token?: String;
+startServer();
+
+async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer<{ token?: String }>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors({
+      credentials: true,
+    }),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  const PORT = parseInt(process?.env?.PORT || "4000");
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
 }
-
-const app = express();
-const httpServer = http.createServer(app);
-const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
-await server.start();
-app.use(
-  "/graphql",
-  cors<cors.CorsRequest>(),
-  express.json(),
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  })
-);
-
-const PORT = parseInt(process?.env?.PORT || "4000");
-
-await new Promise<void>((resolve) =>
-  httpServer.listen({ port: PORT }, resolve)
-);
-console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
